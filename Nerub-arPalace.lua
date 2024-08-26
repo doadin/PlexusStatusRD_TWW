0288232ea621c45a1d68e72ed95cef9759b27187
tbkizle <tbkizle@gmail.com>
2024-08-26 14:03:43 -0400

    Initial Version

---
+++ Nerub-arPalace.lua
@@ -0,0 +1,239 @@
+-- local zone = "Nerub-ar Palace"
+local zoneid = 2292
+
+-- "Ulgrax the Devourer"
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Ulgrax the Devourer")
+GridStatusRaidDebuff:DebuffId(zoneid, 434776, 101, 5, 5, true) --Carnivorous Contest
+GridStatusRaidDebuff:DebuffId(zoneid, 440904, 102, 5, 5, true) --Devour
+GridStatusRaidDebuff:DebuffId(zoneid, 455847, 103, 5, 5, true) --Battered and Bruised
+GridStatusRaidDebuff:DebuffId(zoneid, 440849, 104, 5, 5, true) --Contemptful Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 441451, 105, 5, 5, true) --Stalker's Webbing
+GridStatusRaidDebuff:DebuffId(zoneid, 439419, 106, 5, 5, true) --Stalker's Netting
+GridStatusRaidDebuff:DebuffId(zoneid, 455831, 107, 5, 5, true) --Hardened Netting
+GridStatusRaidDebuff:DebuffId(zoneid, 435138, 108, 5, 5, true) --Digestive Acid
+GridStatusRaidDebuff:DebuffId(zoneid, 435136, 109, 5, 5, true) --Venomous Lash
+GridStatusRaidDebuff:DebuffId(zoneid, 434697, 110, 5, 5, true) --Brutal Crush
+GridStatusRaidDebuff:DebuffId(zoneid, 434705, 111, 5, 5, true) --Tenderized
+GridStatusRaidDebuff:DebuffId(zoneid, 435341, 112, 5, 5, true) --Hulking Crash
+GridStatusRaidDebuff:DebuffId(zoneid, 440177, 113, 5, 5, true) --Ready to Feed
+GridStatusRaidDebuff:DebuffId(zoneid, 438012, 114, 5, 5, true) --Hungering Bellows
+GridStatusRaidDebuff:DebuffId(zoneid, 438041, 115, 5, 5, true) --Insatiable Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 436255, 116, 5, 5, true) --Juggernaut Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 439037, 117, 5, 5, true) --Disembowel
+GridStatusRaidDebuff:DebuffId(zoneid, 438657, 118, 5, 5, true) --Chunky Viscera
+GridStatusRaidDebuff:DebuffId(zoneid, 438324, 119, 5, 5, true) --Feed
+GridStatusRaidDebuff:DebuffId(zoneid, 455870, 120, 5, 5, true) --Bioactive Spines
+GridStatusRaidDebuff:DebuffId(zoneid, 443842, 121, 5, 5, true) --Swallowing Darkness
+
+-- "The Bloodbound Horror"
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "The Bloodbound Horror")
+GridStatusRaidDebuff:DebuffId(zoneid, 444363, 201, 5, 5, true) --Gruesome Disgorge
+GridStatusRaidDebuff:DebuffId(zoneid, 462306, 202, 5, 5, true) --The Unseeming
+GridStatusRaidDebuff:DebuffId(zoneid, 451288, 203, 5, 5, true) --Black Bulwark
+GridStatusRaidDebuff:DebuffId(zoneid, 445016, 204, 5, 5, true) --Spectral Slam
+GridStatusRaidDebuff:DebuffId(zoneid, 445174, 205, 5, 5, true) --Manifest Horror
+GridStatusRaidDebuff:DebuffId(zoneid, 445257, 206, 5, 5, true) --Blood Pact
+GridStatusRaidDebuff:DebuffId(zoneid, 445570, 207, 5, 5, true) --Unseeming Blight
+GridStatusRaidDebuff:DebuffId(zoneid, 452237, 208, 5, 5, true) --Bloodcurdle
+GridStatusRaidDebuff:DebuffId(zoneid, 445936, 209, 5, 5, true) --Spewing Hemorrhage
+GridStatusRaidDebuff:DebuffId(zoneid, 442530, 210, 5, 5, true) --Goresplatter
+GridStatusRaidDebuff:DebuffId(zoneid, 461876, 211, 5, 5, true) --Seeping Transfusion
+GridStatusRaidDebuff:DebuffId(zoneid, 443305, 212, 5, 5, true) --Crimson Rain
+GridStatusRaidDebuff:DebuffId(zoneid, 443042, 213, 5, 5, true) --Grasp From Beyond
+GridStatusRaidDebuff:DebuffId(zoneid, 438696, 214, 5, 5, true) --Black Sepsis
+
+-- "Sikran, Captain of the Sureki"
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Sikran, Captain of the Sureki")
+GridStatusRaidDebuff:DebuffId(zoneid, 433475, 301, 5, 5, true) --Phase Blades
+GridStatusRaidDebuff:DebuffId(zoneid, 458272, 302, 5, 5, true) --Cosmic Simulacrum
+GridStatusRaidDebuff:DebuffId(zoneid, 459273, 303, 5, 5, true) --Cosmic Shards
+GridStatusRaidDebuff:DebuffId(zoneid, 459785, 304, 5, 5, true) --Cosmic Residue
+GridStatusRaidDebuff:DebuffId(zoneid, 461401, 305, 5, 5, true) --Collapsing Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 442428, 306, 5, 5, true) --Decimate
+GridStatusRaidDebuff:DebuffId(zoneid, 456420, 307, 5, 5, true) --Shattering Sweep
+GridStatusRaidDebuff:DebuffId(zoneid, 439511, 308, 5, 5, true) --Captain's Flourish
+GridStatusRaidDebuff:DebuffId(zoneid, 435401, 309, 5, 5, true) --Expose
+GridStatusRaidDebuff:DebuffId(zoneid, 432969, 310, 5, 5, true) --Phase Lunge
+GridStatusRaidDebuff:DebuffId(zoneid, 439559, 311, 5, 5, true) --Rain of Arrows
+
+
+-- "Rasha'nan"
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Rasha'nan")
+GridStatusRaidDebuff:DebuffId(zoneid, 439789, 401, 5, 5, true) --Rolling Acid
+GridStatusRaidDebuff:DebuffId(zoneid, 439785, 402, 5, 5, true) --Corrosion
+GridStatusRaidDebuff:DebuffId(zoneid, 439787, 403, 5, 5, true) --Acidic Stupor
+GridStatusRaidDebuff:DebuffId(zoneid, 439776, 404, 5, 5, true) --Acid Pools
+GridStatusRaidDebuff:DebuffId(zoneid, 439784, 405, 5, 5, true) --Spinneret's Strands
+GridStatusRaidDebuff:DebuffId(zoneid, 439778, 406, 5, 5, true) --Spinneret's Websnap
+GridStatusRaidDebuff:DebuffId(zoneid, 460789, 407, 5, 5, true) --Tacky Threads
+GridStatusRaidDebuff:DebuffId(zoneid, 439780, 408, 5, 5, true) --Sticky Webs
+GridStatusRaidDebuff:DebuffId(zoneid, 439815, 409, 5, 5, true) --Infested Spawn
+GridStatusRaidDebuff:DebuffId(zoneid, 455287, 410, 5, 5, true) --Infested Bite
+GridStatusRaidDebuff:DebuffId(zoneid, 454989, 411, 5, 5, true) --Enveloping Webs
+GridStatusRaidDebuff:DebuffId(zoneid, 444687, 412, 5, 5, true) --Savage Assault
+GridStatusRaidDebuff:DebuffId(zoneid, 458067, 413, 5, 5, true) --Savage Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 439795, 414, 5, 5, true) --Web Reave
+GridStatusRaidDebuff:DebuffId(zoneid, 439811, 415, 5, 5, true) --Erosive Spray
+GridStatusRaidDebuff:DebuffId(zoneid, 440193, 416, 5, 5, true) --Lingering Erosion
+GridStatusRaidDebuff:DebuffId(zoneid, 452806, 417, 5, 5, true) --Acidic Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 457877, 418, 5, 5, true) --Acidic Carapace
+GridStatusRaidDebuff:DebuffId(zoneid, 444094, 419, 5, 5, true) --Caustic Hail
+GridStatusRaidDebuff:DebuffId(zoneid, 439792, 420, 5, 5, true) --Tacky Burst
+
+-- "Broodtwister Ovi'nax"
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "Broodtwister Ovi'nax")
+GridStatusRaidDebuff:DebuffId(zoneid, 442526, 501, 5, 5, true) --Experimental Dosage
+GridStatusRaidDebuff:DebuffId(zoneid, 446694, 502, 5, 5, true) --Mutation: Necrotic
+GridStatusRaidDebuff:DebuffId(zoneid, 438807, 503, 5, 5, true) --Vicious Bite
+GridStatusRaidDebuff:DebuffId(zoneid, 458212, 504, 5, 5, true) --Necrotic Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 446690, 505, 5, 5, true) --Mutation: Ravenous
+GridStatusRaidDebuff:DebuffId(zoneid, 446700, 506, 5, 5, true) --Poison Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 442263, 507, 5, 5, true) --Mutation: Accelerated
+GridStatusRaidDebuff:DebuffId(zoneid, 442251, 508, 5, 5, true) --Fixate
+GridStatusRaidDebuff:DebuffId(zoneid, 442257, 509, 5, 5, true) --Infest
+GridStatusRaidDebuff:DebuffId(zoneid, 442430, 510, 5, 5, true) --Ingest Black Blood
+GridStatusRaidDebuff:DebuffId(zoneid, 450362, 511, 5, 5, true) --Unstable Infusion
+GridStatusRaidDebuff:DebuffId(zoneid, 441612, 512, 5, 5, true) --Vile Discharge
+GridStatusRaidDebuff:DebuffId(zoneid, 452802, 513, 5, 5, true) --Catalyze Mutation
+GridStatusRaidDebuff:DebuffId(zoneid, 442799, 514, 5, 5, true) --Sanguine Overflow
+GridStatusRaidDebuff:DebuffId(zoneid, 450661, 515, 5, 5, true) --Caustic Reaction
+GridStatusRaidDebuff:DebuffId(zoneid, 446349, 516, 5, 5, true) --Sticky Web
+GridStatusRaidDebuff:DebuffId(zoneid, 446351, 517, 5, 5, true) --Web Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 441362, 518, 5, 5, true) --Volatile Concoction
+
+-- "Nexus-Princess Ky'veza"
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "Nexus-Princess Ky'veza")
+GridStatusRaidDebuff:DebuffId(zoneid, 436867, 601, 5, 5, true) --Assassination
+GridStatusRaidDebuff:DebuffId(zoneid, 437343, 602, 5, 5, true) --Queensbane
+GridStatusRaidDebuff:DebuffId(zoneid, 439409, 603, 5, 5, true) --Dark Viscera
+GridStatusRaidDebuff:DebuffId(zoneid, 437620, 604, 5, 5, true) --Nether Rift
+GridStatusRaidDebuff:DebuffId(zoneid, 437786, 605, 5, 5, true) --Atomized
+GridStatusRaidDebuff:DebuffId(zoneid, 439576, 606, 5, 5, true) --Nexus Daggers
+GridStatusRaidDebuff:DebuffId(zoneid, 436950, 607, 5, 5, true) --Stalking Shadows
+GridStatusRaidDebuff:DebuffId(zoneid, 448364, 608, 5, 5, true) --Death Masks
+GridStatusRaidDebuff:DebuffId(zoneid, 447174, 609, 5, 5, true) --Death Cloak
+GridStatusRaidDebuff:DebuffId(zoneid, 438245, 610, 5, 5, true) --Twilight Massacre
+GridStatusRaidDebuff:DebuffId(zoneid, 436749, 611, 5, 5, true) --Reaper
+GridStatusRaidDebuff:DebuffId(zoneid, 440377, 612, 5, 5, true) --Void Shredders
+GridStatusRaidDebuff:DebuffId(zoneid, 440576, 613, 5, 5, true) --Chasmal Gash
+GridStatusRaidDebuff:DebuffId(zoneid, 435414, 614, 5, 5, true) --Starless Night
+GridStatusRaidDebuff:DebuffId(zoneid, 442278, 615, 5, 5, true) --Eternal Night
+GridStatusRaidDebuff:DebuffId(zoneid, 435486, 616, 5, 5, true) --Regicide
+GridStatusRaidDebuff:DebuffId(zoneid, 434645, 617, 5, 5, true) --Eclipse
+
+-- "The Silken Court"
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "The Silken Court")
+GridStatusRaidDebuff:DebuffId(zoneid, 455796, 701, 5, 5, true) --Whispers of The Silken Court
+GridStatusRaidDebuff:DebuffId(zoneid, 455849, 702, 5, 5, true) --Mark of Paranoia
+GridStatusRaidDebuff:DebuffId(zoneid, 460357, 703, 5, 5, true) --Mote of Overwrought Paranoia
+GridStatusRaidDebuff:DebuffId(zoneid, 460359, 704, 5, 5, true) --Void Degeneration
+GridStatusRaidDebuff:DebuffId(zoneid, 455850, 705, 5, 5, true) --Mark of Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 460263, 706, 5, 5, true) --Mote of Unrelenting Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 460281, 707, 5, 5, true) --Burning Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 455863, 708, 5, 5, true) --Mark of Death
+GridStatusRaidDebuff:DebuffId(zoneid, 455363, 709, 5, 5, true) --Queen's Proclamation
+GridStatusRaidDebuff:DebuffId(zoneid, 440158, 710, 5, 5, true) --Reckless Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 460360, 711, 5, 5, true) --Burrowed Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 438801, 712, 5, 5, true) --Call of the Swarm
+GridStatusRaidDebuff:DebuffId(zoneid, 438706, 713, 5, 5, true) --Harden Carapace
+GridStatusRaidDebuff:DebuffId(zoneid, 438773, 714, 5, 5, true) --Shattered Shell
+GridStatusRaidDebuff:DebuffId(zoneid, 455080, 715, 5, 5, true) --Scarab Lord's Perseverance
+GridStatusRaidDebuff:DebuffId(zoneid, 440504, 716, 5, 5, true) --Impaling Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 449857, 717, 5, 5, true) --Impaled
+GridStatusRaidDebuff:DebuffId(zoneid, 438218, 718, 5, 5, true) --Piercing Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 439992, 719, 5, 5, true) --Web Bomb
+GridStatusRaidDebuff:DebuffId(zoneid, 440001, 720, 5, 5, true) --Binding Webs
+GridStatusRaidDebuff:DebuffId(zoneid, 440179, 721, 5, 5, true) --Entangled
+GridStatusRaidDebuff:DebuffId(zoneid, 438656, 722, 5, 5, true) --Venomous Rain
+GridStatusRaidDebuff:DebuffId(zoneid, 450045, 723, 5, 5, true) --Skittering Leap
+GridStatusRaidDebuff:DebuffId(zoneid, 438200, 724, 5, 5, true) --Poison Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 450980, 725, 5, 5, true) --Shatter Existence
+GridStatusRaidDebuff:DebuffId(zoneid, 460600, 726, 5, 5, true) --Entropic Barrage
+GridStatusRaidDebuff:DebuffId(zoneid, 463461, 727, 5, 5, true) --Entropic Vulnerability
+GridStatusRaidDebuff:DebuffId(zoneid, 438677, 728, 5, 5, true) --Stinging Swarm
+GridStatusRaidDebuff:DebuffId(zoneid, 449993, 729, 5, 5, true) --Stinging Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 456235, 730, 5, 5, true) --Stinging Delirium
+GridStatusRaidDebuff:DebuffId(zoneid, 438355, 731, 5, 5, true) --Cataclysmic Entropy
+GridStatusRaidDebuff:DebuffId(zoneid, 441634, 732, 5, 5, true) --Web Vortex
+GridStatusRaidDebuff:DebuffId(zoneid, 450129, 733, 5, 5, true) --Entropic Desolation
+GridStatusRaidDebuff:DebuffId(zoneid, 441782, 734, 5, 5, true) --Strands of Reality
+GridStatusRaidDebuff:DebuffId(zoneid, 450483, 735, 5, 5, true) --Void Step
+GridStatusRaidDebuff:DebuffId(zoneid, 441772, 736, 5, 5, true) --Void Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 451277, 737, 5, 5, true) --Spike Storm
+GridStatusRaidDebuff:DebuffId(zoneid, 460364, 738, 5, 5, true) --Seismic Upheaval
+GridStatusRaidDebuff:DebuffId(zoneid, 463464, 739, 5, 5, true) --Seismic Vulnerability
+GridStatusRaidDebuff:DebuffId(zoneid, 443092, 740, 5, 5, true) --Spike Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 443063, 741, 5, 5, true) --Unleashed Swarm
+
+-- "Queen Ansurek"
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Queen Ansurek")
+GridStatusRaidDebuff:DebuffId(zoneid, 437592, 801, 5, 5, true) --Reactive Toxin
+GridStatusRaidDebuff:DebuffId(zoneid, 438846, 802, 5, 5, true) --Reactive Froth
+GridStatusRaidDebuff:DebuffId(zoneid, 451278, 803, 5, 5, true) --Concentrated Toxin
+GridStatusRaidDebuff:DebuffId(zoneid, 464628, 804, 5, 5, true) --Reaction Trauma
+GridStatusRaidDebuff:DebuffId(zoneid, 464638, 805, 5, 5, true) --Frothy Toxin
+GridStatusRaidDebuff:DebuffId(zoneid, 464643, 806, 5, 5, true) --Lingering Toxin
+GridStatusRaidDebuff:DebuffId(zoneid, 460133, 807, 5, 5, true) --Toxic Reaction
+GridStatusRaidDebuff:DebuffId(zoneid, 438481, 808, 5, 5, true) --Toxic Waves
+GridStatusRaidDebuff:DebuffId(zoneid, 437078, 809, 5, 5, true) --Acid
+GridStatusRaidDebuff:DebuffId(zoneid, 437417, 810, 5, 5, true) --Venom Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 441556, 811, 5, 5, true) --Reaction Vapor
+GridStatusRaidDebuff:DebuffId(zoneid, 439814, 812, 5, 5, true) --Silken Tomb
+GridStatusRaidDebuff:DebuffId(zoneid, 441958, 813, 5, 5, true) --Grasping Silk
+GridStatusRaidDebuff:DebuffId(zoneid, 440899, 814, 5, 5, true) --Liquefy
+GridStatusRaidDebuff:DebuffId(zoneid, 441084, 815, 5, 5, true) --Acid Explosion
+GridStatusRaidDebuff:DebuffId(zoneid, 437093, 816, 5, 5, true) --Feast
+GridStatusRaidDebuff:DebuffId(zoneid, 439299, 817, 5, 5, true) --Web Blades
+GridStatusRaidDebuff:DebuffId(zoneid, 440607, 818, 5, 5, true) --Acrid Presence
+GridStatusRaidDebuff:DebuffId(zoneid, 447076, 819, 5, 5, true) --Predation
+GridStatusRaidDebuff:DebuffId(zoneid, 447170, 820, 5, 5, true) --Predation Threads
+GridStatusRaidDebuff:DebuffId(zoneid, 447456, 821, 5, 5, true) --Paralyzing Venom
+GridStatusRaidDebuff:DebuffId(zoneid, 451607, 822, 5, 5, true) --Paralyzing Waves
+GridStatusRaidDebuff:DebuffId(zoneid, 447411, 823, 5, 5, true) --Wrest
+GridStatusRaidDebuff:DebuffId(zoneid, 447240, 824, 5, 5, true) --Devour
+GridStatusRaidDebuff:DebuffId(zoneid, 460366, 825, 5, 5, true) --Shadowgate
+GridStatusRaidDebuff:DebuffId(zoneid, 464056, 826, 5, 5, true) --Gloom Touch
+GridStatusRaidDebuff:DebuffId(zoneid, 447983, 827, 5, 5, true) --Gloom Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 443403, 828, 5, 5, true) --Gloom
+GridStatusRaidDebuff:DebuffId(zoneid, 460218, 829, 5, 5, true) --Shadowy Distortion
+GridStatusRaidDebuff:DebuffId(zoneid, 448660, 830, 5, 5, true) --Acid Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 449940, 831, 5, 5, true) --Acidic Apocalypse
+GridStatusRaidDebuff:DebuffId(zoneid, 447950, 832, 5, 5, true) --Shadowblast
+GridStatusRaidDebuff:DebuffId(zoneid, 447999, 833, 5, 5, true) --Radiating Gloom
+GridStatusRaidDebuff:DebuffId(zoneid, 448176, 834, 5, 5, true) --Gloom Orbs
+GridStatusRaidDebuff:DebuffId(zoneid, 448046, 835, 5, 5, true) --Gloom Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 448300, 836, 5, 5, true) --Echoing Connection
+GridStatusRaidDebuff:DebuffId(zoneid, 448488, 837, 5, 5, true) --Worshipper's Protection
+GridStatusRaidDebuff:DebuffId(zoneid, 462541, 838, 5, 5, true) --Cosmic Rupture
+GridStatusRaidDebuff:DebuffId(zoneid, 464799, 839, 5, 5, true) --Lingering Fallout
+GridStatusRaidDebuff:DebuffId(zoneid, 462564, 840, 5, 5, true) --Cosmic Fallout
+GridStatusRaidDebuff:DebuffId(zoneid, 448458, 841, 5, 5, true) --Cosmic Apocalypse
+GridStatusRaidDebuff:DebuffId(zoneid, 448147, 842, 5, 5, true) --Oust
+GridStatusRaidDebuff:DebuffId(zoneid, 460315, 843, 5, 5, true) --Ousting Fragments
+GridStatusRaidDebuff:DebuffId(zoneid, 451600, 844, 5, 5, true) --Expulsion Beam
+GridStatusRaidDebuff:DebuffId(zoneid, 455374, 845, 5, 5, true) --Dark Detonation
+GridStatusRaidDebuff:DebuffId(zoneid, 449235, 846, 5, 5, true) --Caustic Fangs
+GridStatusRaidDebuff:DebuffId(zoneid, 443888, 847, 5, 5, true) --Abyssal Infusion
+GridStatusRaidDebuff:DebuffId(zoneid, 443915, 848, 5, 5, true) --Abyssal Conduit
+GridStatusRaidDebuff:DebuffId(zoneid, 455387, 849, 5, 5, true) --Abyssal Reverberation
+GridStatusRaidDebuff:DebuffId(zoneid, 444507, 850, 5, 5, true) --Conduit Ejections
+GridStatusRaidDebuff:DebuffId(zoneid, 444502, 851, 5, 5, true) --Conduit Collapse
+GridStatusRaidDebuff:DebuffId(zoneid, 445422, 852, 5, 5, true) --Frothing Gluttony
+GridStatusRaidDebuff:DebuffId(zoneid, 445623, 853, 5, 5, true) --Glutton Threads
+GridStatusRaidDebuff:DebuffId(zoneid, 445877, 854, 5, 5, true) --Froth Vapor
+GridStatusRaidDebuff:DebuffId(zoneid, 461408, 855, 5, 5, true) --Consume
+GridStatusRaidDebuff:DebuffId(zoneid, 444829, 856, 5, 5, true) --Queen's Summons
+GridStatusRaidDebuff:DebuffId(zoneid, 445152, 857, 5, 5, true) --Acolyte's Essence
+GridStatusRaidDebuff:DebuffId(zoneid, 446012, 858, 5, 5, true) --Essence Scarred
+GridStatusRaidDebuff:DebuffId(zoneid, 445013, 859, 5, 5, true) --Dark Barrier
+GridStatusRaidDebuff:DebuffId(zoneid, 445021, 860, 5, 5, true) --Null Detonation
+GridStatusRaidDebuff:DebuffId(zoneid, 438976, 861, 5, 5, true) --Royal Condemnation
+GridStatusRaidDebuff:DebuffId(zoneid, 441865, 862, 5, 5, true) --Royal Shackles
+GridStatusRaidDebuff:DebuffId(zoneid, 441872, 863, 5, 5, true) --Royal Cocoon
+GridStatusRaidDebuff:DebuffId(zoneid, 443325, 864, 5, 5, true) --Infest
+GridStatusRaidDebuff:DebuffId(zoneid, 443667, 865, 5, 5, true) --Infested Gloomburst
+GridStatusRaidDebuff:DebuffId(zoneid, 443720, 866, 5, 5, true) --Gloom Hatchling
+GridStatusRaidDebuff:DebuffId(zoneid, 443336, 867, 5, 5, true) --Gorge
+GridStatusRaidDebuff:DebuffId(zoneid, 443396, 868, 5, 5, true) --Gloom Splatter
+GridStatusRaidDebuff:DebuffId(zoneid, 445268, 869, 5, 5, true) --Dreadful Presence
+GridStatusRaidDebuff:DebuffId(zoneid, 451832, 870, 5, 5, true) --Cataclysmic Evolution
