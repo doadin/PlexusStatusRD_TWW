5f407647b2fbd2a463f4eb2a354fe0918d714596
tbkizle <tbkizle@gmail.com>
2025-08-12 01:39:14 -0400

    Initial Manaforge Omega Support

---
+++ ManaforgeOmega.lua
@@ -0,0 +1,257 @@
+-- local zone = "Manaforge Omega"
+local zoneid = 2460
+
+-- "Plexus Sentinel"
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Plexus Sentinel")
+GridStatusRaidDebuff:DebuffId(zoneid, 1218148, 101, 5, 5, true) --Phase Blink
+GridStatusRaidDebuff:DebuffId(zoneid, 1217649, 102, 5, 5, true) --Arcanomatrix Atomizer
+GridStatusRaidDebuff:DebuffId(zoneid, 1219223, 103, 5, 5, true) --Atomize
+GridStatusRaidDebuff:DebuffId(zoneid, 1219248, 104, 5, 5, true) --Arcane Radiation
+GridStatusRaidDebuff:DebuffId(zoneid, 1227794, 105, 5, 5, true) --Arcane Lightning
+GridStatusRaidDebuff:DebuffId(zoneid, 1234733, 106, 5, 5, true) --Cleanse the Chamber
+GridStatusRaidDebuff:DebuffId(zoneid, 1219532, 107, 5, 5, true) --Eradicating Salvo
+GridStatusRaidDebuff:DebuffId(zoneid, 1219450, 108, 5, 5, true) --Manifest Matrices
+GridStatusRaidDebuff:DebuffId(zoneid, 1218626, 109, 5, 5, true) --Displacement Matrix
+GridStatusRaidDebuff:DebuffId(zoneid, 1219354, 110, 5, 5, true) --Potent Mana Residue
+GridStatusRaidDebuff:DebuffId(zoneid, 1219263, 111, 5, 5, true) --Obliteration Arcanocannon
+GridStatusRaidDebuff:DebuffId(zoneid, 1223364, 112, 5, 5, true) --Powered Automaton
+GridStatusRaidDebuff:DebuffId(zoneid, 1220489, 113, 5, 5, true) --Protocol: Purge
+GridStatusRaidDebuff:DebuffId(zoneid, 1218669, 114, 5, 5, true) --Energy Cutter
+GridStatusRaidDebuff:DebuffId(zoneid, 1233110, 115, 5, 5, true) --Purging Lightning
+GridStatusRaidDebuff:DebuffId(zoneid, 1219471, 116, 5, 5, true) --Expulsion Zone
+GridStatusRaidDebuff:DebuffId(zoneid, 1235816, 117, 5, 5, true) --Energy Overload
+
+--"Loom'ithar"
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Loom'ithar")
+GridStatusRaidDebuff:DebuffId(zoneid, 1237272, 201, 5, 5, true) --Lair Weaving
+GridStatusRaidDebuff:DebuffId(zoneid, 1238502, 202, 5, 5, true) --Woven Ward
+GridStatusRaidDebuff:DebuffId(zoneid, 1247672, 203, 5, 5, true) --Infusion Pylons
+GridStatusRaidDebuff:DebuffId(zoneid, 1247029, 204, 5, 5, true) --Excess Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 1247045, 205, 5, 5, true) --Hyper Infusion
+GridStatusRaidDebuff:DebuffId(zoneid, 1226315, 206, 5, 5, true) --Infusion Tether
+GridStatusRaidDebuff:DebuffId(zoneid, 1226366, 207, 5, 5, true) --Living Silk
+GridStatusRaidDebuff:DebuffId(zoneid, 1226721, 208, 5, 5, true) --Silken Snare
+GridStatusRaidDebuff:DebuffId(zoneid, 1226395, 209, 5, 5, true) --Overinfusion Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 1226867, 210, 5, 5, true) --Primal Spellstorm
+GridStatusRaidDebuff:DebuffId(zoneid, 1231408, 211, 5, 5, true) --Arcane Overflow
+GridStatusRaidDebuff:DebuffId(zoneid, 1227263, 212, 5, 5, true) --Piercing Strand
+GridStatusRaidDebuff:DebuffId(zoneid, 1231403, 213, 5, 5, true) --Silk Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 1228059, 214, 5, 5, true) --Unbound Rage
+GridStatusRaidDebuff:DebuffId(zoneid, 1243771, 215, 5, 5, true) --Arcane Ichor
+GridStatusRaidDebuff:DebuffId(zoneid, 1227782, 216, 5, 5, true) --Arcane Outrage
+GridStatusRaidDebuff:DebuffId(zoneid, 1227226, 217, 5, 5, true) --Writhing Wave
+GridStatusRaidDebuff:DebuffId(zoneid, 1242303, 218, 5, 5, true) --Writhing Swathe
+
+--"Soulbinder Naazindhri"
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Soulbinder Naazindhri")
+GridStatusRaidDebuff:DebuffId(zoneid, 1225582, 301, 5, 5, true) --Soul Calling
+GridStatusRaidDebuff:DebuffId(zoneid, 1239988, 302, 5, 5, true) --Soulweave
+GridStatusRaidDebuff:DebuffId(zoneid, 1227048, 303, 5, 5, true) --Voidblade Ambush
+GridStatusRaidDebuff:DebuffId(zoneid, 1227052, 304, 5, 5, true) --Void Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 1242018, 305, 5, 5, true) --Void Resonance
+GridStatusRaidDebuff:DebuffId(zoneid, 1235576, 306, 5, 5, true) --Phase Blades
+GridStatusRaidDebuff:DebuffId(zoneid, 1227848, 307, 5, 5, true) --Essence Implosion
+GridStatusRaidDebuff:DebuffId(zoneid, 1227276, 308, 5, 5, true) --Soulfray Annihilation
+GridStatusRaidDebuff:DebuffId(zoneid, 1246530, 309, 5, 5, true) --Arcane Sigils
+GridStatusRaidDebuff:DebuffId(zoneid, 1246775, 310, 5, 5, true) --Shatterpulse
+GridStatusRaidDebuff:DebuffId(zoneid, 1223859, 311, 5, 5, true) --Arcane Expulsion
+GridStatusRaidDebuff:DebuffId(zoneid, 1242086, 312, 5, 5, true) --Arcane Energy
+GridStatusRaidDebuff:DebuffId(zoneid, 1225616, 313, 5, 5, true) --Soulfire Convergence
+GridStatusRaidDebuff:DebuffId(zoneid, 1226827, 314, 5, 5, true) --Soulrend Orb
+GridStatusRaidDebuff:DebuffId(zoneid, 1240754, 315, 5, 5, true) --Spellburn
+GridStatusRaidDebuff:DebuffId(zoneid, 1241100, 316, 5, 5, true) --Mystic Lash
+
+--"Forgeweaver Araz"
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Forgeweaver Araz")
+GridStatusRaidDebuff:DebuffId(zoneid, 1231720, 401, 5, 5, true) --Invoke Collector
+GridStatusRaidDebuff:DebuffId(zoneid, 1228214, 402, 5, 5, true) --Astral Harvest
+GridStatusRaidDebuff:DebuffId(zoneid, 1236207, 403, 5, 5, true) --Astral Surge
+GridStatusRaidDebuff:DebuffId(zoneid, 1237322, 404, 5, 5, true) --Prime Sequence
+GridStatusRaidDebuff:DebuffId(zoneid, 1228103, 405, 5, 5, true) --Arcane Siphon
+GridStatusRaidDebuff:DebuffId(zoneid, 1231726, 406, 5, 5, true) --Arcane Barrier
+GridStatusRaidDebuff:DebuffId(zoneid, 1248171, 407, 5, 5, true) --Void Tear
+GridStatusRaidDebuff:DebuffId(zoneid, 1245640, 408, 5, 5, true) --Power Manifested
+GridStatusRaidDebuff:DebuffId(zoneid, 1228218, 409, 5, 5, true) --Arcane Obliteration
+GridStatusRaidDebuff:DebuffId(zoneid, 1238867, 410, 5, 5, true) --Echoing Invocation
+GridStatusRaidDebuff:DebuffId(zoneid, 1238874, 411, 5, 5, true) --Echoing Tempest
+GridStatusRaidDebuff:DebuffId(zoneid, 1228454, 412, 5, 5, true) --Mark of Power
+GridStatusRaidDebuff:DebuffId(zoneid, 1228219, 413, 5, 5, true) --Astral Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 1228188, 414, 5, 5, true) --Silencing Tempest
+GridStatusRaidDebuff:DebuffId(zoneid, 1228502, 415, 5, 5, true) --Overwhelming Power
+GridStatusRaidDebuff:DebuffId(zoneid, 1227631, 416, 5, 5, true) --Arcane Expulsion
+GridStatusRaidDebuff:DebuffId(zoneid, 1248009, 417, 5, 5, true) --Dark Terminus
+GridStatusRaidDebuff:DebuffId(zoneid, 1240705, 418, 5, 5, true) --Astral Burn
+GridStatusRaidDebuff:DebuffId(zoneid, 1232409, 419, 5, 5, true) --Unstable Surge
+GridStatusRaidDebuff:DebuffId(zoneid, 1234328, 420, 5, 5, true) --Photon Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 1226260, 421, 5, 5, true) --Arcane Convergence
+GridStatusRaidDebuff:DebuffId(zoneid, 1243272, 422, 5, 5, true) --Containment Breach
+GridStatusRaidDebuff:DebuffId(zoneid, 1232738, 423, 5, 5, true) --Hardened Shell
+GridStatusRaidDebuff:DebuffId(zoneid, 1238266, 424, 5, 5, true) --Ramping Power
+GridStatusRaidDebuff:DebuffId(zoneid, 1233415, 425, 5, 5, true) --Mana Splinter
+GridStatusRaidDebuff:DebuffId(zoneid, 1232412, 426, 5, 5, true) --Focusing Iris
+GridStatusRaidDebuff:DebuffId(zoneid, 1233076, 427, 5, 5, true) --Dark Singularity
+GridStatusRaidDebuff:DebuffId(zoneid, 1233074, 428, 5, 5, true) --Crushing Darkness
+GridStatusRaidDebuff:DebuffId(zoneid, 1243901, 429, 5, 5, true) --Void Harvest
+GridStatusRaidDebuff:DebuffId(zoneid, 1243641, 430, 5, 5, true) --Void Surge
+GridStatusRaidDebuff:DebuffId(zoneid, 1232221, 431, 5, 5, true) --Death Throes
+
+--"The Soul Hunters"
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "The Soul Hunters")
+GridStatusRaidDebuff:DebuffId(zoneid, 1222232, 501, 5, 5, true) --Devourer's Ire
+GridStatusRaidDebuff:DebuffId(zoneid, 1234565, 502, 5, 5, true) --Consume
+GridStatusRaidDebuff:DebuffId(zoneid, 1222310, 503, 5, 5, true) --Unending Hunger
+GridStatusRaidDebuff:DebuffId(zoneid, 1227355, 504, 5, 5, true) --Voidstep
+GridStatusRaidDebuff:DebuffId(zoneid, 1227685, 505, 5, 5, true) --Hungering Slash
+GridStatusRaidDebuff:DebuffId(zoneid, 1235045, 506, 5, 5, true) --Encroaching Oblivion
+GridStatusRaidDebuff:DebuffId(zoneid, 1245743, 507, 5, 5, true) --Eradicate
+GridStatusRaidDebuff:DebuffId(zoneid, 1227809, 508, 5, 5, true) --The Hunt
+GridStatusRaidDebuff:DebuffId(zoneid, 1247415, 509, 5, 5, true) --Weakened Prey
+GridStatusRaidDebuff:DebuffId(zoneid, 1241306, 510, 5, 5, true) --Blade Dance
+GridStatusRaidDebuff:DebuffId(zoneid, 1218103, 511, 5, 5, true) --Eye Beam
+GridStatusRaidDebuff:DebuffId(zoneid, 1221490, 512, 5, 5, true) --Fel-Singed
+GridStatusRaidDebuff:DebuffId(zoneid, 1225127, 513, 5, 5, true) --Felblade
+GridStatusRaidDebuff:DebuffId(zoneid, 1223725, 514, 5, 5, true) --Fel Inferno
+GridStatusRaidDebuff:DebuffId(zoneid, 1241833, 515, 5, 5, true) --Fracture
+GridStatusRaidDebuff:DebuffId(zoneid, 1226493, 516, 5, 5, true) --Shattered Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 1241917, 517, 5, 5, true) --Frailty
+GridStatusRaidDebuff:DebuffId(zoneid, 1242259, 518, 5, 5, true) --Spirit Bomb
+GridStatusRaidDebuff:DebuffId(zoneid, 1242284, 519, 5, 5, true) --Soulcrush
+GridStatusRaidDebuff:DebuffId(zoneid, 1242304, 520, 5, 5, true) --Expulsed Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 1240891, 521, 5, 5, true) --Sigil of Chains
+GridStatusRaidDebuff:DebuffId(zoneid, 1225154, 522, 5, 5, true) --Immolation Aura
+GridStatusRaidDebuff:DebuffId(zoneid, 1249198, 523, 5, 5, true) --Unstable Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 1233093, 524, 5, 5, true) --Collapsing Star
+GridStatusRaidDebuff:DebuffId(zoneid, 1233105, 525, 5, 5, true) --Dark Residue
+GridStatusRaidDebuff:DebuffId(zoneid, 1233968, 526, 5, 5, true) --Event Horizon
+GridStatusRaidDebuff:DebuffId(zoneid, 1245978, 527, 5, 5, true) --Soul Tether
+GridStatusRaidDebuff:DebuffId(zoneid, 1233863, 528, 5, 5, true) --Fel Rush
+GridStatusRaidDebuff:DebuffId(zoneid, 1227113, 529, 5, 5, true) --Infernal Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 1227117, 530, 5, 5, true) --Fel Devastation
+GridStatusRaidDebuff:DebuffId(zoneid, 1233381, 531, 5, 5, true) --Withering Flames
+
+--"Fractillus"
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "Fractillus")
+GridStatusRaidDebuff:DebuffId(zoneid, 1233657, 601, 5, 5, true) --Nether Prism
+GridStatusRaidDebuff:DebuffId(zoneid, 1226089, 602, 5, 5, true) --Crystal Nexus
+GridStatusRaidDebuff:DebuffId(zoneid, 1232130, 603, 5, 5, true) --Nexus Shrapnel
+GridStatusRaidDebuff:DebuffId(zoneid, 1236784, 604, 5, 5, true) --Brittle Nexus
+GridStatusRaidDebuff:DebuffId(zoneid, 1232760, 605, 5, 5, true) --Crystal Lacerations
+GridStatusRaidDebuff:DebuffId(zoneid, 1236785, 606, 5, 5, true) --Void-Infused Nexus
+GridStatusRaidDebuff:DebuffId(zoneid, 1247424, 607, 5, 5, true) --Null Consumption
+GridStatusRaidDebuff:DebuffId(zoneid, 1247495, 608, 5, 5, true) --Null Explosion
+GridStatusRaidDebuff:DebuffId(zoneid, 1233917, 609, 5, 5, true) --Crystaline Overcharge
+GridStatusRaidDebuff:DebuffId(zoneid, 1224414, 610, 5, 5, true) --Crystalline Shockwave
+GridStatusRaidDebuff:DebuffId(zoneid, 1220394, 611, 5, 5, true) --Shattering Backhand
+GridStatusRaidDebuff:DebuffId(zoneid, 1227373, 612, 5, 5, true) --Shattershell
+GridStatusRaidDebuff:DebuffId(zoneid, 1227378, 613, 5, 5, true) --Crystal Encasement
+GridStatusRaidDebuff:DebuffId(zoneid, 1231871, 614, 5, 5, true) --Shockwave Slam
+
+--"Nexus-King Salhadaar"
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "Nexus-King Salhadaar")
+GridStatusRaidDebuff:DebuffId(zoneid, 1224731, 701, 5, 5, true) --Decree: Oath-Bound
+GridStatusRaidDebuff:DebuffId(zoneid, 1224767, 702, 5, 5, true) --King's Thrall
+GridStatusRaidDebuff:DebuffId(zoneid, 1224764, 703, 5, 5, true) --Oath-Breaker
+GridStatusRaidDebuff:DebuffId(zoneid, 1224906, 704, 5, 5, true) --Invoke the Oath
+GridStatusRaidDebuff:DebuffId(zoneid, 1238975, 705, 5, 5, true) --Vengeful Oath
+GridStatusRaidDebuff:DebuffId(zoneid, 1224776, 706, 5, 5, true) --Subjugation Rule
+GridStatusRaidDebuff:DebuffId(zoneid, 1224787, 707, 5, 5, true) --Conquer
+GridStatusRaidDebuff:DebuffId(zoneid, 1224812, 708, 5, 5, true) --Vanquish
+GridStatusRaidDebuff:DebuffId(zoneid, 1227529, 709, 5, 5, true) --Banishment
+GridStatusRaidDebuff:DebuffId(zoneid, 1224822, 710, 5, 5, true) --Tyranny
+GridStatusRaidDebuff:DebuffId(zoneid, 1225099, 711, 5, 5, true) --Fractal Images
+GridStatusRaidDebuff:DebuffId(zoneid, 1247215, 712, 5, 5, true) --Fractal Claw
+GridStatusRaidDebuff:DebuffId(zoneid, 1224827, 713, 5, 5, true) --Behead
+GridStatusRaidDebuff:DebuffId(zoneid, 1231097, 714, 5, 5, true) --Cosmic Rip
+GridStatusRaidDebuff:DebuffId(zoneid, 1227330, 715, 5, 5, true) --Besiege
+GridStatusRaidDebuff:DebuffId(zoneid, 1227891, 716, 5, 5, true) --Coalesce Voidwing
+GridStatusRaidDebuff:DebuffId(zoneid, 1228113, 717, 5, 5, true) --Netherbreaker
+GridStatusRaidDebuff:DebuffId(zoneid, 1228163, 718, 5, 5, true) --Dimension Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 1234539, 719, 5, 5, true) --Dimension Glare
+GridStatusRaidDebuff:DebuffId(zoneid, 1234529, 720, 5, 5, true) --Cosmic Maw
+GridStatusRaidDebuff:DebuffId(zoneid, 1228065, 721, 5, 5, true) --Rally the Shadowguard
+GridStatusRaidDebuff:DebuffId(zoneid, 1230302, 722, 5, 5, true) --Self-Destruct
+GridStatusRaidDebuff:DebuffId(zoneid, 1232399, 723, 5, 5, true) --Dread Mortar
+GridStatusRaidDebuff:DebuffId(zoneid, 1237105, 724, 5, 5, true) --Twilight Barrier
+GridStatusRaidDebuff:DebuffId(zoneid, 1228075, 725, 5, 5, true) --Nexus Beams
+GridStatusRaidDebuff:DebuffId(zoneid, 1230261, 726, 5, 5, true) --Netherblast
+GridStatusRaidDebuff:DebuffId(zoneid, 1237107, 727, 5, 5, true) --Twilight Massacre
+GridStatusRaidDebuff:DebuffId(zoneid, 1250044, 728, 5, 5, true) --Taking Aim
+GridStatusRaidDebuff:DebuffId(zoneid, 1228053, 729, 5, 5, true) --Reap
+GridStatusRaidDebuff:DebuffId(zoneid, 1232327, 730, 5, 5, true) --Seal the Forge
+GridStatusRaidDebuff:DebuffId(zoneid, 1228284, 731, 5, 5, true) --Royal Ward
+GridStatusRaidDebuff:DebuffId(zoneid, 1228265, 732, 5, 5, true) --King's Hunger
+GridStatusRaidDebuff:DebuffId(zoneid, 1226648, 733, 5, 5, true) --Galactic Smash
+GridStatusRaidDebuff:DebuffId(zoneid, 1248137, 734, 5, 5, true) --Dark Star
+GridStatusRaidDebuff:DebuffId(zoneid, 1225444, 735, 5, 5, true) --Atomized
+GridStatusRaidDebuff:DebuffId(zoneid, 1225645, 736, 5, 5, true) --Twilight Spikes
+GridStatusRaidDebuff:DebuffId(zoneid, 1226384, 737, 5, 5, true) --Dark Orbit
+GridStatusRaidDebuff:DebuffId(zoneid, 1226879, 738, 5, 5, true) --Stars Collide
+GridStatusRaidDebuff:DebuffId(zoneid, 1234906, 739, 5, 5, true) --Nexus Collapse
+GridStatusRaidDebuff:DebuffId(zoneid, 1226362, 740, 5, 5, true) --Twilight Scar
+GridStatusRaidDebuff:DebuffId(zoneid, 1226417, 741, 5, 5, true) --Starshattered
+GridStatusRaidDebuff:DebuffId(zoneid, 1226347, 742, 5, 5, true) --Starkiller Swing
+GridStatusRaidDebuff:DebuffId(zoneid, 1226042, 743, 5, 5, true) --Starkiller Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 1225634, 744, 5, 5, true) --World in Twilight
+
+--"Dimensius, the All-Devouring"
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Dimensius, the All-Devouring")
+GridStatusRaidDebuff:DebuffId(zoneid, 1229327, 801, 5, 5, true) --Oblivion
+GridStatusRaidDebuff:DebuffId(zoneid, 1230087, 802, 5, 5, true) --Massive Smash
+GridStatusRaidDebuff:DebuffId(zoneid, 1231005, 803, 5, 5, true) --Fission
+GridStatusRaidDebuff:DebuffId(zoneid, 1228206, 804, 5, 5, true) --Excess Mass
+GridStatusRaidDebuff:DebuffId(zoneid, 1228207, 805, 5, 5, true) --Collective Gravity
+GridStatusRaidDebuff:DebuffId(zoneid, 1230168, 806, 5, 5, true) --Mortal Fragility
+GridStatusRaidDebuff:DebuffId(zoneid, 1248240, 807, 5, 5, true) --Infinite Possibilities
+GridStatusRaidDebuff:DebuffId(zoneid, 1229038, 808, 5, 5, true) --Devour
+GridStatusRaidDebuff:DebuffId(zoneid, 1229674, 809, 5, 5, true) --Growing Hunger
+GridStatusRaidDebuff:DebuffId(zoneid, 1230979, 810, 5, 5, true) --Dark Matter
+GridStatusRaidDebuff:DebuffId(zoneid, 1231002, 811, 5, 5, true) --Dark Energy
+GridStatusRaidDebuff:DebuffId(zoneid, 1243690, 812, 5, 5, true) --Shattered Space
+GridStatusRaidDebuff:DebuffId(zoneid, 1243704, 813, 5, 5, true) --Antimatter
+GridStatusRaidDebuff:DebuffId(zoneid, 1243699, 814, 5, 5, true) --Spatial Fragment
+GridStatusRaidDebuff:DebuffId(zoneid, 1243577, 815, 5, 5, true) --Reverse Gravity
+GridStatusRaidDebuff:DebuffId(zoneid, 1243609, 816, 5, 5, true) --Airborne
+GridStatusRaidDebuff:DebuffId(zoneid, 1250614, 817, 5, 5, true) --Anomalous Force
+GridStatusRaidDebuff:DebuffId(zoneid, 1227665, 818, 5, 5, true) --Fists of the Voidlord
+GridStatusRaidDebuff:DebuffId(zoneid, 1228367, 819, 5, 5, true) --Cosmic Radiation
+GridStatusRaidDebuff:DebuffId(zoneid, 1235114, 820, 5, 5, true) --Soaring Reshii
+GridStatusRaidDebuff:DebuffId(zoneid, 1235467, 821, 5, 5, true) --Umbral Gate
+GridStatusRaidDebuff:DebuffId(zoneid, 1241188, 822, 5, 5, true) --Endless Darkness
+GridStatusRaidDebuff:DebuffId(zoneid, 1237080, 823, 5, 5, true) --Broken World
+GridStatusRaidDebuff:DebuffId(zoneid, 1235490, 824, 5, 5, true) --Astrophysical Jet
+GridStatusRaidDebuff:DebuffId(zoneid, 1232987, 825, 5, 5, true) --Black Hole
+GridStatusRaidDebuff:DebuffId(zoneid, 1230674, 826, 5, 5, true) --Spaghettification
+GridStatusRaidDebuff:DebuffId(zoneid, 1246930, 827, 5, 5, true) --Stellar Core
+GridStatusRaidDebuff:DebuffId(zoneid, 1246948, 828, 5, 5, true) --Shooting Star
+GridStatusRaidDebuff:DebuffId(zoneid, 1238765, 829, 5, 5, true) --Extinction
+GridStatusRaidDebuff:DebuffId(zoneid, 1237319, 830, 5, 5, true) --Gamma Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 1234242, 831, 5, 5, true) --Gravitational Distortion
+GridStatusRaidDebuff:DebuffId(zoneid, 1234243, 832, 5, 5, true) --Crushing Gravity
+GridStatusRaidDebuff:DebuffId(zoneid, 1234251, 833, 5, 5, true) --Crushed
+GridStatusRaidDebuff:DebuffId(zoneid, 1234244, 834, 5, 5, true) --Inverse Gravity
+GridStatusRaidDebuff:DebuffId(zoneid, 1237690, 835, 5, 5, true) --Eclipse
+GridStatusRaidDebuff:DebuffId(zoneid, 1237694, 836, 5, 5, true) --Mass Ejection
+GridStatusRaidDebuff:DebuffId(zoneid, 1237696, 837, 5, 5, true) --Debris Field
+GridStatusRaidDebuff:DebuffId(zoneid, 1249423, 838, 5, 5, true) --Mass Destruction
+GridStatusRaidDebuff:DebuffId(zoneid, 1239262, 839, 5, 5, true) --Conqueror's Cross
+GridStatusRaidDebuff:DebuffId(zoneid, 1239270, 840, 5, 5, true) --Voidwarding
+GridStatusRaidDebuff:DebuffId(zoneid, 1246537, 841, 5, 5, true) --Entropic Unity
+GridStatusRaidDebuff:DebuffId(zoneid, 1246541, 842, 5, 5, true) --Null Binding
+GridStatusRaidDebuff:DebuffId(zoneid, 1249248, 843, 5, 5, true) --Boundless
+GridStatusRaidDebuff:DebuffId(zoneid, 1246143, 844, 5, 5, true) --Touch of Oblivion
+GridStatusRaidDebuff:DebuffId(zoneid, 1237695, 845, 5, 5, true) --Stardust Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 1249454, 846, 5, 5, true) --Starshard Nova
+GridStatusRaidDebuff:DebuffId(zoneid, 1254385, 847, 5, 5, true) --Starshard
+GridStatusRaidDebuff:DebuffId(zoneid, 1254384, 848, 5, 5, true) --Star Burst
+GridStatusRaidDebuff:DebuffId(zoneid, 1245292, 849, 5, 5, true) --Destabilized
+GridStatusRaidDebuff:DebuffId(zoneid, 1233292, 850, 5, 5, true) --Accretion Disk
+GridStatusRaidDebuff:DebuffId(zoneid, 1231716, 851, 5, 5, true) --Extinguish The Stars
+GridStatusRaidDebuff:DebuffId(zoneid, 1232394, 852, 5, 5, true) --Gravity Well
+GridStatusRaidDebuff:DebuffId(zoneid, 1248479, 853, 5, 5, true) --Stellar Overload
+GridStatusRaidDebuff:DebuffId(zoneid, 1233557, 854, 5, 5, true) --Density
+GridStatusRaidDebuff:DebuffId(zoneid, 1232973, 855, 5, 5, true) --Supernova
+GridStatusRaidDebuff:DebuffId(zoneid, 1234052, 856, 5, 5, true) --Darkened Sky
+GridStatusRaidDebuff:DebuffId(zoneid, 1234054, 857, 5, 5, true) --Shadowquake
+GridStatusRaidDebuff:DebuffId(zoneid, 1234263, 858, 5, 5, true) --Cosmic Collapse
+GridStatusRaidDebuff:DebuffId(zoneid, 1234266, 859, 5, 5, true) --Cosmic Fragility
+GridStatusRaidDebuff:DebuffId(zoneid, 1250055, 860, 5, 5, true) --Voidgrasp
